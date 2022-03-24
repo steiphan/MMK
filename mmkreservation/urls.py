60e83c8fc0359a4503edc1f56bec75a72917d0f3
steiphan <93961031+steiphan@users.noreply.github.com>
2022-03-24 10:27:26 +0800

    okay

--- mmkreservation/urls.py
+++ mmkreservation/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from mmkreservation import views
 
 app_name = 'mmkreservation'
 
@@ -27,4 +30,5 @@ urlpatterns= [
 	##Dashboard
 	path('userviewroom', views.UserViewRoom.as_view(), name="userviewroom_view"),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- mmmkhotel/settings.py
+++ mmmkhotel/settings.py
@@ -127,13 +127,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    "mmkreservation\static"
-]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'mmkreservation/static'),
+
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
